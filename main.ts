c0aa14858921b3649a02fa8d9cf4fd7a329aa329
dantef12 <100299384+dantef12@users.noreply.github.com>
2022-03-01 10:53:22 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,30 @@
+input.onButtonPressed(Button.A, function () {
+    number_1 += 1
+    basic.showNumber(number_1)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.clearScreen()
+    basic.showNumber(number_2)
+    basic.showNumber(number_1)
+})
+input.onButtonPressed(Button.B, function () {
+    number_2 += 1
+    basic.showNumber(number_2)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.clearScreen()
+    number_1 = 0
+    number_2 = 0
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.clearScreen()
+    basic.showNumber(number_1)
+    basic.showNumber(number_2)
+})
+let number_2 = 0
+let number_1 = 0
+number_1 = 0
+number_2 = 0
 basic.forever(function () {
 	
 })
