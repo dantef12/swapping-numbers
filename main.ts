7dd0c6c1ac2ab2d224fd5ae6efcb8434c8e0c47e
dantef12 <100299384+dantef12@users.noreply.github.com>
2022-03-03 11:11:51 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,30 +1,45 @@
 input.onButtonPressed(Button.A, function () {
-    number_1 += 1
-    basic.showNumber(number_1)
+    if (_switch == 0) {
+        number_1 += 1
+        basic.showNumber(number_1)
+    }
+    if (_switch == 1) {
+        number_2 += 1
+        basic.showNumber(number_2)
+    }
 })
 input.onButtonPressed(Button.AB, function () {
-    basic.clearScreen()
-    basic.showNumber(number_2)
-    basic.showNumber(number_1)
+    if (_switch == 0) {
+        _switch += 1
+        number_1 = number_1
+        basic.clearScreen()
+    }
+    if (_switch == 1) {
+        basic.showNumber(number_2)
+        basic.showString("and")
+        basic.showNumber(number_1)
+    }
 })
 input.onButtonPressed(Button.B, function () {
-    number_2 += 1
-    basic.showNumber(number_2)
+    if (_switch == 0) {
+        number_1 += -1
+        basic.showNumber(number_1)
+    }
+    if (_switch == 1) {
+        number_2 += -1
+        basic.showNumber(number_2)
+    }
 })
 input.onGesture(Gesture.Shake, function () {
     basic.clearScreen()
     number_1 = 0
     number_2 = 0
+    basic.showIcon(IconNames.Heart)
 })
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    basic.clearScreen()
-    basic.showNumber(number_1)
-    basic.showNumber(number_2)
-})
+let _switch = 0
 let number_2 = 0
 let number_1 = 0
+basic.showIcon(IconNames.Skull)
 number_1 = 0
 number_2 = 0
-basic.forever(function () {
-	
-})
+_switch = 0
